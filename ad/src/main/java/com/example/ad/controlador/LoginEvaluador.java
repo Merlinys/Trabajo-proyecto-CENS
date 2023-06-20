466e93e128890e13d3041039a7f422d6f493d933
Martin Fonseca <martinlucianofonsecarubio@gmail.com>
2023-06-20 00:07:14 -0400

    cambio menor idiota 0:07

--- ad/src/main/java/com/example/ad/controlador/LoginEvaluador.java
+++ ad/src/main/java/com/example/ad/controlador/LoginEvaluador.java
@@ -3,6 +3,7 @@ package com.example.ad.controlador;
 import com.example.ad.persistance.CrudrepositorioEntity.IRepositorioEvaluadorEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,8 +14,9 @@ public class LoginEvaluador {
     private final IRepositorioEvaluadorEntity iRepositorioEvaluadorEntity;
 
     @PostMapping("/LoginEvaluador")
-    public String login(@RequestParam(value = "IdEvaluador") Integer id, @RequestParam(value = "pass") String pass) {
+    public String login(@RequestParam(value = "IdEvaluador") Integer id, @RequestParam(value = "pass") String pass, Model model) {
         if ((iRepositorioEvaluadorEntity.findByIdEvaluadorAndContraseña(id,pass)).isPresent()){
+            model.addAttribute("userId", id);
             return "optEVAL";
         }
         else{
--- ad/src/main/java/com/example/ad/controlador/OpcionesEvaluador.java
+++ ad/src/main/java/com/example/ad/controlador/OpcionesEvaluador.java
@@ -2,7 +2,6 @@ package com.example.ad.controlador;
 
 import com.example.ad.domain.servicios.IRepEmpresaServicio;
 import com.example.ad.persistance.entities.RepEmpresa;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
+//Controlador que maneja las opciones del evaluador
 
 @Controller
 public class OpcionesEvaluador {
@@ -24,6 +24,7 @@ public class OpcionesEvaluador {
     }
 
 
+    //maneja la parte para seleccionar las empresas para evaluar , tambien maneja como funciona el crud.
     @GetMapping("/evaluarEmpresa")
     public String opcion(Model model){
         List<RepEmpresa> empresas = repEmpresaServicio.listar();
